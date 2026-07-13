293fb4c9518bc924e34610bc5cc3eb6948da952c
Arturo Vasquez <arturados@gmail.com>
2026-07-13 03:24:19 -0400

    feat: migracion a PostgreSQL

--- database.py
+++ database.py
@@ -3,7 +3,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./rescate.db")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=5, max_overflow=10)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
